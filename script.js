0883e073e9b52b74930057f52e776b4cde3e504b
Leonard Lee <Leonard133@users.noreply.github.com>
2023-01-03 02:55:00 +0800

    new version

--- script.js
+++ script.js
@@ -6,36 +6,72 @@ var toNow = now.getTime();
 var clacLoveDay = (toNow - toFirst) / (1000 * 60 * 60 * 24);
 var loveDay = Math.floor(clacLoveDay) + 1;
 
-document.querySelector("#accent").innerHTML = loveDay + " days";
+var start = moment();
+var end = moment().add(moment().diff(moment("2022-10-27"), 'days'), 'days');
 
-// calc anniversary : days
-function calcDays(days) {
-    var dayms = toFirst + days * (1000 * 60 * 60 * 24);
-    var DAY = new Date(dayms);
-    var year = DAY.getFullYear();
-    var month = DAY.getMonth() + 1;
-    var date = DAY.getDate() - 1;
+var years = end.diff(start, 'year');
+start.add(years, 'years');
+var months = end.diff(start, 'months');
+start.add(months, 'months');
+var days = end.diff(start, 'days');
 
-    document.querySelector(`#date${days}`).innerHTML = `${year}. ${month}. ${date}`;
-}
+var fullDay = "";
+if(years > 0)
+    fullDay += years + '年';
+if(months > 0)
+    fullDay += months + '个月';
+if(days > 0)
+    fullDay += days + '天';
 
-// calc anniversary : years
-function calcYearDays(yearDays) {
-    var dayms = toFirst + yearDays * (1000 * 60 * 60 * 24);
-    var DAY = new Date(dayms);
-    var year = DAY.getFullYear();
-    var month = DAY.getMonth() + 1;
-    var date = DAY.getDate();
+document.querySelector("#accent").innerHTML = fullDay;
 
-    document.querySelector(`#date${yearDays}`).innerHTML = `${year}. ${month}. ${date}`;
-}
-
-
-// execute function
-calcDays(100);
-calcDays(200);
-calcDays(300);
-calcDays(400);
-calcDays(500);
-
-calcYearDays(365);
+var array = [
+    {
+        days: 100,
+        type: 'days'
+    },
+    {
+        days: 200,
+        type: 'days'
+    },
+    {
+        days: 1,
+        type: 'years'
+    },
+    {
+        days: 500,
+        type: 'days'
+    },
+    {
+        days: 2,
+        type: 'years'
+    },
+    {
+        days: 3,
+        type: 'years'
+    },
+    {
+        days: 4,
+        type: 'years'
+    },
+    {
+        days: 5,
+        type: 'years'
+    }
+];
+var html = "";
+array.forEach(element => {
+    var nextDate = moment(firstDay).add(element.days, element.type);
+    console.log(nextDate)
+    html += `<li>
+    <div class="px-4 py-2 sm:px-6">
+      <div class="flex items-center justify-between">
+        <p class="truncate text-sm font-medium">${element.days}${(element.type=='days') ? "天" : '年'}</p>
+        <div class="ml-2 flex flex-shrink-0">
+          <p class="inline-flex px-2 text-xs font-semibold leading-5">${nextDate.format('YYYY-MM-DD')}</p>
+        </div>
+      </div>
+    </div>
+</li>`;
+});
+document.querySelector("#anniversary").innerHTML = html;
